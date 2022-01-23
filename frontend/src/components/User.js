885aecd1985da96e4ccc47a0806d784a0cb03059
Andrey Hurpia da Rocha <ahrocha@gmail.com>
2022-01-23 12:53:24 -0300

    show user info

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -2,12 +2,28 @@ import React from 'react';
 import apiClient from '../services/api';
 
 const User = () => {
+    const [logged, setLogged] = React.useState(false);
+    const [user, setUser] = React.useState([]);
     React.useEffect(() => {
         apiClient.get('http://localhost:8080/api/user')
             .then(response => {
+                console.log(response);
+                console.log(response.status);
                 console.log(response.data);
+                if (response.status == 200) {
+                    setLogged(true);
+                    setUser(response.data);
+                    console.log(response.data);
+                }
             });
     }, []);
+    if (logged === true) {
+        return (
+            <div>
+            <h1>User: {user.name} </h1>
+            </div>
+        );
+    }
     return (
         <div>
         <h1>User</h1>
