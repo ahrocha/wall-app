64bb5cece45976ed964172716103608d5cbb2948
Andrey Hurpia da Rocha <ahrocha@gmail.com>
2022-01-25 20:50:32 -0300

    UI improved after register

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -4,11 +4,14 @@ import { Navigate } from 'react-router-dom';
 
 const Register = (props) => {
     const [home, setHome] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
     const [name, setName] = React.useState('');
     const [password_confirmation, setPasswordConfirmation] = React.useState('');
+
     const handleSubmit = (e) => {
+        setLoading(true);
         e.preventDefault();
         apiClient.get('/sanctum/csrf-cookie')
             .then(response => {
@@ -21,7 +24,9 @@ const Register = (props) => {
                             name: name
                         }).then(response => {
                             if (response.status === 201) {
+                                console.log('recebeu 201');
                                 setHome(true);
+                                setLoading(false);
                             }
                         }).catch(error => {
                             if (error.response) {
@@ -36,71 +41,82 @@ const Register = (props) => {
                                 if (errors.length > 0) {
                                     alert(errors.join(' '));
                                 } else {
-                                    alert ('Unknown error.');
+                                    alert('Unknown error.');
                                 }
                             } else if (error.request) {
                                 alert('Error accessing the backend.');
                             } else {
-                                alert('Error ' + error.message);
+                                alert('Error: ' + error.message);
                             }
+                            setLoading(false);
                         });
                     });
             });
     }
+    if (loading === true) {
+        return (
+            <div>
+                <h1>Registering</h1>
+                <p>Loading. This can take a while because SMTP is slooooooow.</p>
+            </div>
+        )
+    }
     if (home === true) {
-        <div>
-            <h1>Registered</h1>
-            <p>An welcome email was sent. Please check your inbox.</p>
-        </div>
+        return (
+            <div>
+                <h1>Registered</h1>
+                <p>An welcome email was sent. Please check your inbox. Or the spam.</p>
+            </div>
+        )
     }
     return (
         <div>
             <h1>Register</h1>
             <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-                <label htmlFor="email" className="form-label">Email address</label>
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="email"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                    required
-                    className="form-control"
-                />
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        required
+                        className="form-control"
+                    />
                 </div><div className="mb-3">
-                <label htmlFor="name" className="form-label">Name</label>
-                <input
-                    type="name"
-                    name="name"
-                    placeholder="Your name"
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                    required
-                    className="form-control"
-                />
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input
+                        type="name"
+                        name="name"
+                        placeholder="Your name"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        required
+                        className="form-control"
+                    />
                 </div><div className="mb-3">
-                <label htmlFor="password" className="form-label">Password</label>
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    required
-                    className="form-control"
-                />
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        required
+                        className="form-control"
+                    />
                 </div><div className="mb-3">
-                <label htmlFor="password_confirmation" className="form-label">Confirm the password</label>
-                <input
-                    type="password"
-                    name="password_confirmation"
-                    placeholder="Confirm your password"
-                    value={password_confirmation}
-                    onChange={e => setPasswordConfirmation(e.target.value)}
-                    required
-                    className="form-control"
-                />
+                    <label htmlFor="password_confirmation" className="form-label">Confirm the password</label>
+                    <input
+                        type="password"
+                        name="password_confirmation"
+                        placeholder="Confirm your password"
+                        value={password_confirmation}
+                        onChange={e => setPasswordConfirmation(e.target.value)}
+                        required
+                        className="form-control"
+                    />
                 </div>
                 <button type="submit" className='btn btn-primary'>Register</button>
             </form>
