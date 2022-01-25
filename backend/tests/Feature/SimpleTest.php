53b9eebe7027eb79af9fed25c9545e7fe5f3b14a
Andrey Hurpia da Rocha <ahrocha@gmail.com>
2022-01-25 13:44:18 -0300

    Tests for endpoints

---
+++ backend/tests/Feature/SimpleTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+
+class SimpleTest extends TestCase
+{
+    /**
+     *
+     * @return void
+     */
+    public function test_guest_can_list_posts()
+    {
+
+        $response = $this->withHeaders([
+            'Accept' => 'application/json'
+        ])->get('/api/post');
+        $response->assertStatus(200)
+            ->assertJsonStructure([[
+                'id',
+                'post',
+                'user_id',
+                'created_at',
+                'updated_at',
+            ]]);
+    }
+
+    /**
+     *
+     * @return void
+     */
+    public function test_user_login_and_navigate()
+    {
+        $response = $this->withHeaders([
+            'Accept' => 'application/json'
+        ])->postJson('/login', ['email' => 'ahrocha@gmail.com', 'password' => 'secret123']);
+
+        $this->assertAuthenticated();
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'two_factor' => false,
+            ]);
+
+        $response = $this->withHeaders([
+            'Accept' => 'application/json'
+        ])->get('/api/user');
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'id',
+                'name',
+                'email',
+                'created_at',
+                'updated_at',
+            ]);
+
+        $response = $this->withHeaders([
+            'Accept' => 'application/json'
+        ])->postJson('/api/post', [
+            'post' => 'no random post text with random numbers ' . rand(1111, 99999)
+        ]);
+        $response->assertStatus(201);
+
+        $response = $this->withHeaders([
+            'Accept' => 'application/json'
+        ])->get('/api/post');
+        $response->assertStatus(200)
+            ->assertJsonStructure([[
+                'id',
+                'post',
+                'user_id',
+                'created_at',
+                'updated_at',
+            ]]);
+
+        $response = $this->withHeaders([
+            'Accept' => 'application/json'
+        ])->postJson('/logout');
+        $response->assertStatus(204);
+    }
+
+    /**
+     *
+     * @return void
+     */
+    public function test_guest_cant_see_profile()
+    {
+        $response = $this->withHeaders([
+            'Accept' => 'application/json'
+        ])->get('/api/user');
+        $response->assertStatus(401);
+        $response->assertJson([
+            'message' => "Unauthenticated.",
+        ]);
+    }
+
+    /**
+     *
+     * @return void
+     */
+    public function test_guest_cant_post()
+    {
+        $response = $this->withHeaders([
+            'Accept' => 'application/json'
+        ])->postJson('/api/post', [
+            'post' => 'no random post text with random numbers ' . rand(1111, 99999)
+        ]);
+        $response->assertStatus(401);
+        $response->assertJson([
+            'message' => "Unauthenticated.",
+        ]);
+    }
+}
