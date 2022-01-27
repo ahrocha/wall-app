5985c6199be339ca07fd733d083090f6debaf0f7
Andrey Hurpia da Rocha <ahrocha@gmail.com>
2022-01-26 21:13:32 -0300

    No post message.

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -2,24 +2,34 @@ import React from 'react';
 import apiClient from '../services/api';
 
 const Posts = () => {
-    
+    const [loading, setLoading] = React.useState(false);
     const [posts, setPosts] = React.useState([]);
     React.useEffect(() => {
+        setLoading(true);
         apiClient.get('/sanctum/csrf-cookie')
             .then(response => {
                 apiClient.get('/api/post')
                     .then(response => {
                         setPosts(response.data);
+                        setLoading(false);
                     });
             });
     }, []);
-    if (posts.length == 0) {
+    if (posts.length === 0 && loading === true) {
         return (
             <div>
                 <h1>Loading posts...</h1>
             </div>
         );
-    }
+    } 
+
+    if (posts.length === 0 && loading === false) {
+        return (
+            <div>
+                <h1>There isn't a post. You should write one.</h1>
+            </div>
+        );
+    } 
     const postList = posts.map((post) =>
         <div key={post.id} className='card'>
             <div className='card-body'>
