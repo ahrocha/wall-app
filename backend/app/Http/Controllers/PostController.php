dfe1b6bcd920453cfff7f00e66103e92dff73061
Andrey Hurpia da Rocha <ahrocha@gmail.com>
2022-01-27 10:23:14 -0300

    Order by adjust at PostController

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
     {
         $return = Post::with(['user' => function($query) {
             $query->select('id','name');
-        }])->get();
+        }])->orderByDesc('id')->get();
         return response()->json($return);
     }
 
